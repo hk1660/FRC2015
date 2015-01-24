5d2bb457da2cb33f1bf171bf2c1f16012d6ddf58
Dummy user <a@b.com>
2015-01-24 15:28:18 -0500

    Initial drivertrain for mechanum driving

--- CAN Talon SRX/src/org/usfirst/frc/team1660/robot/Robot.java
+++ CAN Talon SRX/src/org/usfirst/frc/team1660/robot/Robot.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team1660.robot;
 
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.SampleRobot;
@@ -10,36 +11,40 @@ import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import com.kauailabs.nav6.frc.IMU; 
-import com.kauailabs.nav6.frc.IMUAdvanced;
-
-
-
-
+//import com.kauailabs.nav6.frc.IMU; 
+//import com.kauailabs.nav6.frc.IMUAdvanced;
 
 /**
  * This is a short sample program demonstrating how to use the basic throttle
  * mode of the new CAN Talon.
  *  Testing commit
  */
-public class hkRobot extends SampleRobot {
+public class Robot extends SampleRobot {
+	
+	RobotDrive hkDrive;
+	Joystick driverStick;
+	
+	public void robotInit() {
+		hkDrive = new RobotDrive(frontleft, backleft, frontright, backright);
+	    driverStick = new Joystick(1);
+	}
 
 	//Code forthe NavX Gyro
-    SerialPort serial_port;
-    IMUAdvanced imu;      //IMU imu;  // Alternatively, use IMUAdvanced for advanced features
+  //  SerialPort serial_port;
+    //IMUAdvanced imu;      //IMU imu;  // Alternatively, use IMUAdvanced for advanced features
     //SmartDashboard dash;
     boolean first_iteration;
 
     //Assigning motor names to CANTalons
-  CANTalon motor1;
-  CANTalon motor2;
-  CANTalon motor3;
-  CANTalon motor4;
+  CANTalon frontleft;
+  CANTalon frontright;
+  CANTalon backleft;
+  CANTalon backright;
 
-  public hkRobot() {
+  public Robot() {
 	  
 	  //Code for the NavX Gyro
-
+/**
     
 	   	try {
 	    	serial_port = new SerialPort(57600,SerialPort.Port.kOnboard);
@@ -53,7 +58,7 @@ public class hkRobot extends SampleRobot {
 			// features.
 			
 			byte update_rate_hz = 50;
-			//imu = new IMU(serial_port,update_rate_hz);
+			imu = new IMU(serial_port,update_rate_hz);
 			imu = new IMUAdvanced(serial_port,update_rate_hz);
 	    	} catch( Exception ex ) {
 	    		
@@ -63,8 +68,6 @@ public class hkRobot extends SampleRobot {
 	        }
 	        first_iteration = true;
 	  
-  
-  
       	SmartDashboard.putNumber(   "IMU_Yaw", 			imu.getYaw());
   		SmartDashboard.putNumber(   "IMU_Pitch",            imu.getPitch());
         SmartDashboard.putNumber(   "IMU_Roll",             imu.getRoll());
@@ -72,34 +75,54 @@ public class hkRobot extends SampleRobot {
         SmartDashboard.putNumber(   "IMU_Accel_Y",          imu.getWorldLinearAccelY());
         SmartDashboard.putBoolean(  "IMU_IsMoving",         imu.isMoving());
 	  
-	  
-      motor1 = new CANTalon(1); // Initialize the CanTalonSRX on device 1.
-      motor2 = new CANTalon(2);
-      motor3 = new CANTalon(3);
-      motor4 = new CANTalon(4);
+	**/  
+      frontleft = new CANTalon(1); // Initialize the CanTalonSRX on device 1.
+      backleft = new CANTalon(2);
+      backright = new CANTalon(3);
+      frontright = new CANTalon(4);
   }
 
   /**
     * Runs the motor.
     */
+  
+ public void checkJoystick()
+ {
+	 double threshold = 0.11;
+	 
+	 double x = driverStick.getRawAxis(4) ;
+	 double moveValue = driverStick.getRawAxis(1);
+	 double rotateValue = driverStick.getRawAxis(0);
+	
+	if(moveValue > threshold*-1 && moveValue < threshold) {
+		moveValue = 0;
+	}
+	if(rotateValue > threshold*-1 && rotateValue < threshold) {
+		rotateValue = 0;
+	}
+	
+	if(x > threshold*-1 && x < threshold) {
+		x = 0;
+	}
+	
+	System.out.println("move: "+moveValue+" rotate: "+rotateValue);
+	
+	hkDrive.mecanumDrive_Cartesian(rotateValue, moveValue, x, 0);
+	 
+ }
+ 
   public void operatorControl() {
     while (isOperatorControl() && isEnabled()) {
-      // Set the motor's output to half power.
-      // This takes a number from -1 (100% speed in reverse) to +1 (100% speed
-      // going forward)
-      motor1.set(0.5);
-      motor2.set(0.5);
-      motor3.set(0.5);
-      motor4.set(0.5);
-
+    	checkJoystick();	
+    	
       Timer.delay(0.01);  // Note that the CANTalon only receives updates every
                           // 10ms, so updating more quickly would not gain you
                           // anything.
     }
-    motor1.disable();
-    motor2.disable();
-    motor3.disable();
-    motor4.disable();
+    frontleft.disable();
+    frontright.disable();
+    backleft.disable();
+    backright.disable();
   }
 }
 
